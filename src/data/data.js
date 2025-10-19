7636ab046ae3ab971165070ea2012b285c83ef7a
soylltari <mmmsribranets@gmail.com>
2025-10-19 15:52:49 +0300

    feat: implement all user endpoints

--- src/data/data.js
+++ src/data/data.js
@@ -1,4 +1,7 @@
-let users = {};
+let users = {
+  "1d1asd1": { id: "1d1asd1", name: "Test1" },
+  "2a1sd5d": { id: "2a1sd5d", name: "Test2" },
+};
 
 const { v4: uuidv4 } = require("uuid");
 
@@ -13,7 +16,21 @@ const getUsers = () => {
   return Object.values(users);
 };
 
+const getUserByID = (id) => {
+  return users[id];
+};
+
+const deleteUser = (id) => {
+  if (users[id]) {
+    delete users[id];
+    return true;
+  }
+  return false;
+};
+
 module.exports = {
   createUser,
   getUsers,
+  getUserByID,
+  deleteUser,
 };
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -13,4 +13,22 @@ router.get("/", (req, res) => {
   res.status(200).json(userList);
 });
 
+router.get("/:id", (req, res) => {
+  const user = data.getUserByID(req.params.id);
+  if (!user) {
+    return res.status(404).json({ error: "User is not found" });
+  }
+  res.status(200).json(user);
+});
+
+router.delete("/:id", async (req, res) => {
+  const wasDeleted = data.deleteUser(req.params.id);
+
+  if (!wasDeleted) {
+    return res.status(404).json({ error: "User is not found" });
+  } else {
+    res.json({ message: "User was deleted successfully" });
+  }
+});
+
 module.exports = router;
